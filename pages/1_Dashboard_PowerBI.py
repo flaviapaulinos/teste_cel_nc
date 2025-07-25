af40a186c4d3782a4ce68df5a9450b4e0c73fd19
Flavia BPScht <112699827+flaviabpscht@users.noreply.github.com>
2025-07-25 16:27:18 -0300

    larguradash

--- pages/1_Dashboard_PowerBI.py
+++ pages/1_Dashboard_PowerBI.py
@@ -14,7 +14,14 @@ with st.container():
     # Link do Power BI
     powerbi_link = "https://app.powerbi.com/view?r=eyJrIjoiYTE0NTliNjQtMTYzMC00MDZmLTgyODgtMTE5Y2UwOTc2MjQ2IiwidCI6ImU5YTgyZWM3LTRhODYtNDNkZS1hYjJhLTcxOWQ2Njk1OWExYiJ9"
 
-    components.iframe(powerbi_link, width=100%, height=561)  # Tamanho ampliado
+    st.markdown(
+    f"""
+    <div style="display: flex; justify-content: center;">
+        <iframe title="PowerBI" width="100%" height="561" src="{powerbi_link}" frameborder="0" allowFullScreen="true"></iframe>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
         
 
 # Rodapé
