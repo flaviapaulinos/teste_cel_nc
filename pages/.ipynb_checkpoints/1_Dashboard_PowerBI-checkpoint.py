b663719641a239e3d801d34bab736f8ba7cce475
Flavia BPScht <112699827+flaviabpscht@users.noreply.github.com>
2025-07-25 16:24:06 -0300

    readme

--- pages/.ipynb_checkpoints/1_Dashboard_PowerBI-checkpoint.py
+++ pages/.ipynb_checkpoints/1_Dashboard_PowerBI-checkpoint.py
@@ -14,7 +14,7 @@ with st.container():
     # Link do Power BI
     powerbi_link = "https://app.powerbi.com/view?r=eyJrIjoiYTE0NTliNjQtMTYzMC00MDZmLTgyODgtMTE5Y2UwOTc2MjQ2IiwidCI6ImU5YTgyZWM3LTRhODYtNDNkZS1hYjJhLTcxOWQ2Njk1OWExYiJ9"
 
-    components.iframe(powerbi_link, width=900, height=561)  # Tamanho ampliado
+    components.iframe(powerbi_link, width=100%, height=561)  # Tamanho ampliado
         
 
 # Rodapé
